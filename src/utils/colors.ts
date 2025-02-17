b40345a1a6278025d14813bb15f1a95eddf4a424
Albert Caloracan <albert.caloracan@gmail.com>
2025-02-17 17:02:25 +0800

    Add color constants

---
+++ src/utils/colors.ts
@@ -0,0 +1,10 @@
+export const COLORS = {
+  red: '#ec5f5f',
+  text: '#8C8C8C',
+  black: '#303030',
+  grey: '#C7C9D9',
+  background: '#F6F7FA',
+  iconGray: '#BDC1C4',
+  placeholder: '#9D9FA0',
+  white: '#FFF',
+};
